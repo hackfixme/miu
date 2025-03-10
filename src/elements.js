2667fbe58df3941305edfcb68bfa11afed3a84a5
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2025-03-10 21:46:05 +0100

    fix(elements): efficient array rendering
    This ensures that elements under a for loop are only created when
    needed. Previously the for loop tree was recreated on every change,
    which messed with event handlers. Now event handlers are only created
    once, elements are reused, and discarded when not needed.

--- src/elements.js
+++ src/elements.js
@@ -1,46 +1,59 @@
 import { StoreRegistry } from './store.js';
 
 const MIU_ATTRS = {
-  BIND: 'data-miu-bind',
-  FOR: 'data-miu-for',
-  ON: 'data-miu-on'
+  BIND:  'data-miu-bind',
+  FOR:   'data-miu-for',
+  INDEX: 'data-miu-index',
+  ON:    'data-miu-on',
 };
 
-// Context passed to event handlers within a for loop.
-// It includes the element and its index within the loop.
-const bindingContexts = new WeakMap();
+// Mapping of parent for-loop element to items rendered in the loop.
+// Used to pass the correct element index to child event handlers.
+const bindContexts = new WeakMap();
 
-function processBindings(root, store, context = null) {
-  root.querySelectorAll(`[${MIU_ATTRS.BIND}]`).forEach(element => {
-    // TODO: Support JSON path
-    const path = element.getAttribute(MIU_ATTRS.BIND);
-    if (context) {
-      // For array elements
-      if (element.tagName === 'INPUT') {
-        element.value = context[path] || '';
-        if (element.type === 'checkbox') {
-          element.checked = context[path] || false;
-          element.addEventListener('change', (e) => {
-            context[path] = e.target.checked;
-          });
-        } else {
-          element.addEventListener('input', (e) => {
-            context[path] = e.target.value;
-          });
-        }
-      } else {
-        element.textContent = context[path] || '';
-      }
-    } else {
-      // Normal binding
-      if (element.tagName === 'INPUT') {
-        bindingUtil.bindInput(element, store, path);
-      } else {
-        bindingUtil.bindText(element, store, path);
-      }
-    }
+// Mapping of elements to a map of event names to event handlers. Ensures the
+// handlers are only added once.
+const eventHandlers = new WeakMap();
+
+// Maping of elements to a map of store paths to store unsubscribe functions.
+// Ensures the subscriptions are only added once.
+const storeSubs = new WeakMap();
+
+// Wait for DOM updates. Similar to more complex versions in Vue and Alpine.
+// See https://alpinejs.dev/magics/nextTick
+const nextTick = () => new Promise(resolve => setTimeout(resolve, 0));
+
+// Setup data bindings and update DOM element values.
+function setupBindings(root, store) {
+  // Setup for loops first as they create new elements.
+  root.querySelectorAll(`[${MIU_ATTRS.FOR}]`).forEach(element => {
+    bindingUtil.bindForEach(element, store);
   });
 
+  // Setup all bindings, using a context if within a for loop.
+  root.querySelectorAll(`[${MIU_ATTRS.BIND}]`).forEach(element => {
+    bindingUtil.bindElement(element, store);
+  });
+}
+
+// Return context data for elements within a for loop. The data includes the
+// item and item index within the array.
+function getBindContext(element) {
+  const forParent = element.closest(`[${MIU_ATTRS.FOR}]`);
+  const parentContext = bindContexts.get(forParent);
+  const idx = element.closest(`[${MIU_ATTRS.INDEX}]`)?.getAttribute(MIU_ATTRS.INDEX);
+
+  let context = {};
+  if (parentContext && idx) {
+    context = { item: parentContext[idx], index: idx };
+  }
+
+  return context;
+}
+
+// Attach event handlers to child elements of root. The handler method name is
+// retrieved from the `data-miu-on` attribute and is expected to exist on the store.
+function setupEventHandlers(root, store) {
   root.querySelectorAll(`[${MIU_ATTRS.ON}]`).forEach(element => {
     const binding = element.getAttribute(MIU_ATTRS.ON);
     const [eventName, methodName] = binding.split(':');
@@ -54,64 +67,151 @@ function processBindings(root, store, context = null) {
       console.warn(`Method "${methodName}" not found in store`);
     }
 
-    element.addEventListener(eventName, (event) => {
+    addEventHandler(element, eventName, (event) => {
       event.preventDefault();
-      const bindingContext = bindingContexts.get(event.target);
-      store[methodName].call(store, event, bindingContext);
+      const context = getBindContext(element);
+      store[methodName].call(store, event, context);
     });
   });
 }
 
+// Attach the handler function for the event on the element. A handler for a
+// unique event name will only be added once.
+// FIXME: It should be possible to add multiple handlers for the same event...
+function addEventHandler(element, event, handler) {
+  if (!eventHandlers.has(element)) {
+    eventHandlers.set(element, new Map());
+  }
+  const elementEventHandlers = eventHandlers.get(element);
+  if (!elementEventHandlers.get(event)) {
+    element.addEventListener(event, handler);
+    elementEventHandlers.set(event, handler);
+  }
+}
+
+function storeSubscribe(element, path, subFn) {
+  if (!storeSubs.has(element)) {
+    storeSubs.set(element, new Map());
+  }
+  const elementStoreSubs = storeSubs.get(element);
+  if (!elementStoreSubs.get(path)) {
+    const unsub = subFn();
+    elementStoreSubs.set(element, unsub);
+  }
+}
+
 // Utilities for handling element bindings
 const bindingUtil = {
-  bindInput(element, store, path) {
-    element.value = store[path] || '';
-    element.addEventListener('input', (e) => {
-      store[path] = e.target.value;
-    });
-    store.subscribe(path, (value) => {
-      if (element.value !== value) {
-        element.value = value;
+  bindElement(element, store) {
+    // TODO: Support JSON path.
+    const path = element.getAttribute(MIU_ATTRS.BIND);
+    const context = getBindContext(element);
+
+    if (element.tagName === 'INPUT') {
+      if (element.type === 'checkbox') {
+        this.bindCheckbox(element, store, path, context);
+      } else {
+        this.bindInput(element, store, path, context);
       }
+    } else {
+      this.bindText(element, store, path, context);
+    }
+  },
+
+  bindInput(element, store, path, context) {
+    const newVal = (context?.item ?? store)[path] ?? '';
+    if (element.value !== newVal) {
+      element.value = newVal;
+    }
+    addEventHandler(element, 'input', (e) => {
+      (context?.item ?? store)[path] = e.target.value;
+    });
+    storeSubscribe(element, path, () => {
+      store.subscribe(path, (value) => {
+        if (element.value !== value) {
+          element.value = value;
+        }
+      });
     });
   },
 
-  bindText(element, store, path) {
-    element.textContent = store[path] || '';
-    store.subscribe(path, (value) => {
-      element.textContent = value;
+  bindCheckbox(element, store, path, context) {
+    const newVal = (context?.item ?? store)[path] ?? false;
+    if (element.checked !== newVal) {
+      element.checked = newVal;
+    }
+    addEventHandler(element, 'change', (e) => {
+      (context?.item ?? store)[path] = e.target.checked;
+    });
+    storeSubscribe(element, path, () => {
+      store.subscribe(path, (value) => {
+        if (element.checked !== value) {
+          element.checked = value;
+        }
+      });
     });
   },
 
-  bindForEach(element, store, path) {
+  bindText(element, store, path, context) {
+    const newVal = (context?.item ?? store)[path] ?? '';
+    if (element.textContent !== newVal) {
+      element.textContent = newVal;
+    }
+    storeSubscribe(element, path, () => {
+      store.subscribe(path, (value) => {
+        if (element.textContent !== value) {
+          element.textContent = value;
+        }
+      });
+    });
+  },
+
+  bindForEach(element, store) {
     const template = element.querySelector('template');
     if (!template) {
       console.error(`${MIU_ATTRS.FOR} requires a template element`);
       return;
     }
 
+    const path = element.getAttribute(MIU_ATTRS.FOR);
+
     const render = (items) => {
-      // Preserve the template
-      element.textContent = '';
-      element.appendChild(template);
-
-      if (Array.isArray(items)) {
-        items.forEach((item, index) => {
-          const clone = document.importNode(template.content, true);
-
-          // Store context for all elements in this template instance
-          clone.querySelectorAll('*').forEach(el => {
-            bindingContexts.set(el, { item, index });
-          });
-
-          processBindings(clone, store, item);
-          element.appendChild(clone);
-        });
+      if (!Array.isArray(items)) {
+        console.error(`Value of "${path}" is not an array`);
+        return;
       }
+
+      // Set the items rendered under this element. This is used to pass the
+      // items and their index to all child event handlers.
+      bindContexts.set(element, items);
+
+      // Remove excess elements
+      while (element.children.length - 1 > items.length) {
+        element.lastElementChild.remove();
+      }
+
+      items.forEach((item, index) => {
+        const el = element.children[index+1]; // +1 to account for the template
+        if (!el) {
+          // No element for this item, so create it.
+          const clone = document.importNode(template.content, true);
+          element.appendChild(clone);
+          const child = element.lastElementChild;
+          // Set the index of this item so that it can be passed to all child
+          // event handlers.
+          child.setAttribute(MIU_ATTRS.INDEX, index);
+          setupBindings(child, store);
+          setupEventHandlers(child, store);
+        } else {
+          // The element exists, so just update its index and bindings.
+          el.setAttribute(MIU_ATTRS.INDEX, index);
+          setupBindings(el, store);
+        }
+      });
     };
 
-    render(store[path]);
     store.subscribe(path, render);
+    render(store[path]);
   }
 };
 
@@ -133,20 +233,18 @@ class MiuElement extends HTMLElement {
         throw new Error('No store found');
       }
 
-      // Process for bindings first
-      this.querySelectorAll(`[${MIU_ATTRS.FOR}]`).forEach(element => {
-        const path = element.getAttribute(MIU_ATTRS.FOR);
-        bindingUtil.bindForEach(element, this.store, path);
-      });
+      setupBindings(this, this.store);
+      setupEventHandlers(this, this.store);
 
-      // Process regular bindings
-      processBindings(this, this.store);
-
+      await nextTick(); // Wait for DOM updates
       this.dispatchEvent(new CustomEvent('storeready'));
     } catch (err) {
       console.error(`Error connecting to store:`, err);
     }
   }
+
+  // TODO: Implement
+  // disconnectedCallback() {}
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'store' && oldValue && oldValue !== newValue && this.isConnected) {
--- src/miu.test.js
+++ src/miu.test.js
@@ -40,4 +40,53 @@ describe('Miu data binding', () => {
     input.dispatchEvent(new Event('input'));
     expect(store.value).toBe('after');
   });
+
+  test('array elements are reactive', async () => {
+    const storeID = randomString();
+    const store = new Store(`test-${storeID}`, {
+      items: [
+        { text: 'item1' },
+        { text: 'item2' },
+      ],
+      removeItem(event, context) {
+        this.items.splice(context.index, 1);
+      }
+    });
+
+    document.body.innerHTML = `
+      <miu-el store="test-${storeID}">
+        <ul data-miu-for="items">
+          <template>
+            <li>
+              <span data-miu-bind="text"></span>
+              <button data-miu-on="click:removeItem">×</button>
+            </li>
+          </template>
+        </ul>
+      </miu-el>
+    `;
+    await customElements.whenDefined('miu-el');
+
+    // Check initial render
+    let items = document.querySelectorAll('li');
+    expect(items.length).toBe(2);
+    expect(items[0].querySelector('span').textContent).toBe('item1');
+    expect(items[1].querySelector('span').textContent).toBe('item2');
+
+    // Test store mutation
+    store.items.push({ text: 'item3' });
+    items = document.querySelectorAll('li');
+    expect(items.length).toBe(3);
+    expect(items[0].querySelector('span').textContent).toBe('item1');
+    expect(items[1].querySelector('span').textContent).toBe('item2');
+    expect(items[2].querySelector('span').textContent).toBe('item3');
+
+    // Test UI deletion
+    items[1].querySelector('button').click();
+    expect(store.items.length).toBe(2);
+    items = document.querySelectorAll('li');
+    expect(items.length).toBe(2);
+    expect(items[0].querySelector('span').textContent).toBe('item1');
+    expect(items[1].querySelector('span').textContent).toBe('item3');
+  });
 });
