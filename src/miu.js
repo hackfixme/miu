d0306fed2045029bac999a5348acc7f5f45bf6f3
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2025-04-01 13:11:03 +0200

    feat: export library version
    This uses the Git version when the bundle is built, otherwise it falls
    back on the version imported from the package.json. It seems hacky, but
    works well from my tests in Chromium.
    
    Browser support for import attributes is decent, except in Firefox,
    where it's not supported at all[1]. But since I'm developing exclusively
    on Chromium, and the built miu.min.js will not include this (esbuild
    tree-shaking works well), I'm fine with keeping it.
    
    [1]: https://caniuse.com/mdn-javascript_statements_import_import_attributes_type_json

--- src/miu.js
+++ src/miu.js
@@ -1,2 +1,5 @@
 export { Store } from './store.js';
 export { bind } from './bind.js';
+import pkg from '../package.json' with { type: 'json' };
+// GIT_VERSION will be set at build time.
+export const VERSION = typeof GIT_VERSION !== 'undefined' ? GIT_VERSION : pkg.version;
